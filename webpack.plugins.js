f90c9f4a0ad9db794ba40013aa7ac9fdb59d4652
Evan-Pei <1075247853@qq.com>
2022-09-20 16:01:35 +0800

    webpack版本修改

---
+++ webpack.plugins.js
@@ -0,0 +1,27 @@
+/**
+ * @Author : Evan-Pei
+ * @Date : 2022/9/20
+ * @Version : 1.0
+ * @Last Modified time : 2022/9/20
+ **/
+const Webpack = require('webpack')
+const childProcess=require('child_process')
+const userInfo=require("./package.json")
+
+function getProjectInfo() {
+    return `
+    Project name:${userInfo.name}
+    Branch:${childProcess.execSync('git symbolic-ref --short -q HEAD').toString().trim()}
+    Version:${childProcess.execSync('git show -s --format=%h').toString().trim()}
+    Author:${childProcess.execSync('git show -s --format=%cn').toString().trim()}
+    Recent edit by ${childProcess.execSync('git show -s --format=%cn').toString().trim()}/${childProcess.execSync('git show -s --format=%ce').toString().trim()} on ${new Date().toLocaleString()}`
+}
+module.exports=function(){
+    let plugins=[
+        new Webpack.BannerPlugin({
+            banner:getProjectInfo(),
+            exclude:[/vendor/,/manifest/,/common/]
+        })
+    ]
+    return plugins
+}
